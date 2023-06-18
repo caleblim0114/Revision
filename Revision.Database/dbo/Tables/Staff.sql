57694a928f385680674c9343ca97248dca125ebc
Caleb <Caleb@DESKTOP-1OUCQIH>
2023-06-18 21:25:38 +0800

    Add project files.

---
+++ Revision.Database/dbo/Tables/Staff.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Staff]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(MAX) NULL, 
+    [Age] NVARCHAR(MAX) NULL, 
+    [Position] NVARCHAR(MAX) NULL, 
+    [Contact Number] NVARCHAR(MAX) NULL, 
+    [Email] NVARCHAR(MAX) NULL
+)
